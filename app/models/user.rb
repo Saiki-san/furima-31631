3367cfaaff15faf3b1511ca166097f6a8e01b7e2
Saiki-san <saiki_shinnya@yahoo.co.jp>
2020-12-07 01:54:42 +0900

    SNS認証新規登録・ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise:database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable,
+        :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   # アソシエーション
   has_many :items            # 1人のユーザーは、たくさんの商品を出品できる -->
   has_many :purchase_records # 1人のユーザーは、たくさんの商品を購入できる -->
+  has_many :sns_credentials # sns管理モデルとの紐付け
 
   # バリデーション
   with_options presence: true do
@@ -25,4 +27,19 @@ class User < ApplicationRecord
     validates_format_of :password, with: PASSWORD_REGEX #, message: 'には英字と数字の両方を含めて設定してください'
   end
 
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email:    auth.info.email
+    )
+    # userが登録済みであるか判断
+    if user.persisted?
+    sns.user = user
+    sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
