a3ca3de66efefc0b5e2c8854eb2a590362d3c4b9
Saiki-san <saiki_shinnya@yahoo.co.jp>
2020-12-07 21:43:12 +0900

    config/unicorn.rbの記述変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,25 +1,31 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+# app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)  # 「../」が一つ増えている
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1 # 「worker」とは、分裂したUnicornの全プロセスのこと # プロセスを分裂させることで、リクエストに対してのレスポンスを高速にできます。
 # worker_processesという設定項目で、workerの数を決定
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+# working_directory app_path
+working_directory "#{app_path}/current"  # 「current」を指定
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
+# pid "#{app_path}/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"  # 「shared」の中を参照するよう変更
 
 #ポート番号を指定
 # listen 3000
-listen "#{app_path}/tmp/sockets/unicorn.sock" # Nginxを介した処理に変更したため、Unicornの設定も修正
+# listen "#{app_path}/tmp/sockets/unicorn.sock" # Nginxを介した処理に変更したため、Unicornの設定も修正
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"  # 「shared」の中を参照するよう変更
 
 #エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+# stderr_path "#{app_path}/log/unicorn.stderr.log"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"  # 「shared」の中を参照するよう変更
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+# stdout_path "#{app_path}/log/unicorn.stdout.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"  # 「shared」の中を参照するよう変更
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
